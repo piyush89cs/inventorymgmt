ad0c1056a76b96d75a3112f2dde61304222ead00
Piyush Agrawal <piyush89cs@gmail.com>
2021-11-22 13:45:02 +0530

    Initial Commit

---
+++ src/test/java/com/mgmt/inventorymgmt/InventorymgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmt.inventorymgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorymgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
